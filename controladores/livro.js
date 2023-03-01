f13560dfaa51c1788a1e9eab21904ac4e51764f9
Arydiane <arydianejardim@gmail.com>
2023-03-01 20:25:03 -0300

    refatora controladores adiciona validação de id

--- controladores/livro.js
+++ controladores/livro.js
@@ -14,8 +14,14 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id
-        const livro = getLivroPorId(id)
-        res.send(livro)
+
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
 
     } catch (error) {
         res.status(500)
@@ -30,21 +36,26 @@ function postLivro(req, res) {
         res.status(201)
         res.send("Livro inserido com sucesso!")
 
-    } catch(error) {
+    } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
 
-function patchLivro( req, res) {
+function patchLivro(req, res) {
     try {
-       const id = req.params.id
-       const body = req.body
+        const id = req.params.id
 
-       modificaLivro(body, id)
-       res.send("Item modificado com sucesso")
+        if (id && Number(id)) {
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
 
-    } catch(error) {
+    } catch (error) {
         res.status(500)
         res.send(error.message)
     }
@@ -53,19 +64,25 @@ function patchLivro( req, res) {
 function deleteLivro(req, res) {
     try {
         const id = req.params.id
-        deletaLivroPorId(id)
-        res.send("Livro deletado com sucesso!")
 
-    } catch(error) {
+        if( id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("Livro deletado com sucesso!")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+
+    } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
 
 module.exports = {
-    getLivros, 
-    getLivro, 
-    postLivro, 
-    patchLivro, 
+    getLivros,
+    getLivro,
+    postLivro,
+    patchLivro,
     deleteLivro
 }
