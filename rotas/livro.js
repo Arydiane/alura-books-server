d8999d3acfa6d2a50bd1a732c6c4fc23a60947e0
Arydiane <arydianejardim@gmail.com>
2023-03-01 18:25:18 -0300

    refatora e cria controlador para rota getLivros

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,8 +1,19 @@
 const { Router } = require("express")
+const { getLivros} = require("../controladores/livro")
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send("Olá mundo , alura do brasil!")
+router.get('/', getLivros)
+
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
 })
 
 module.exports = router
