7841d0427d75863425fd55343eecefcbb75cf5cd
Arydiane <arydianejardim@gmail.com>
2023-05-04 20:28:33 -0300

    cria o serviço insere favorito

--- servicos/favorito.js
+++ servicos/favorito.js
@@ -10,8 +10,16 @@ function deletaFavoritoPorId(id) {
     fs.writeFileSync("favoritos.json", JSON.stringify(livrosFiltrados))
 }
 
+function insereFavorito(id) {
+    const livros = JSON.parse( fs.readFileSync("livros.json"))
+    const favoritos = JSON.parse( fs.readFileSync("favoritos.json"))
+    const livroInserido = livros.find(livro => livro.id === id)
+    const novaListaDeFavoritos = [...favoritos, livroInserido]
+    fs.writeFileSync("favoritos.json", JSON.stringify(novaListaDeFavoritos))
+}
 
 module.exports = {
     getTodosFavoritos, 
-    deletaFavoritoPorId
+    deletaFavoritoPorId, 
+    insereFavorito
 }
